3cf162ebb33679264476f9a88983b00be8d5309d
Jarppe <jarppe@metosin.fi>
2018-04-02 19:41:27 +0300

    Fix asset path.

--- src/clj/syksy/web/index.clj
+++ src/clj/syksy/web/index.clj
@@ -21,13 +21,13 @@
        [:meta {:charset "utf-8"}]
        [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
        [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
-       (page/include-css "/css/style.css")]
+       (page/include-css "/asset/css/style.css")]
       [:body
        [:div#app
         [:div.loading
          [:h1 loading]]]
        [:div#dev]
-       (page/include-js "/js/main.js")])))
+       (page/include-js "/asset/js/main.js")])))
 
 ;;
 ;; Index handler:
