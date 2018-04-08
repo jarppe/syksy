1581018674e3fc480b37f34eaf68269ec87bf9a1
Jarppe <jarppe@metosin.fi>
2018-04-08 19:56:00 +0300

    Use compound keys, this allows app to reuse components.

--- src/clj/syksy/core.clj
+++ src/clj/syksy/core.clj
@@ -3,17 +3,19 @@
             [ring.util.http-response :as resp]
             [syksy.web.index :as index]))
 
-(defn default-components [{:keys [index-body routes ctx middleware]}]
-  {:syksy.web.server/server {:handlers [(ig/ref :syksy.web.api/handler)
-                                        (ig/ref :syksy.web.index/handler)
-                                        (ig/ref :syksy.web.resources/handler)
-                                        (ig/ref :syksy.web.not-found/handler)]}
-   :syksy.web.resources/handler {}
-   :syksy.web.not-found/handler {}
-   :syksy.web.index/handler {:index-body (or index-body (index/index {}))}
-   :syksy.web.api/handler {:routes (or routes
-                                       (-> {:message "Syksy is ready"}
-                                           (resp/ok)
-                                           (constantly)))
-                           :ctx ctx
-                           :middleware middleware}})
+(defn default-components [{:keys [index-body routes ctx middleware handlers addon-handlers]}]
+  {[:syksy.web.server/server ::syksy] {:handlers (or handlers
+                                                    (concat [(ig/ref [:syksy.web.api/handler ::syksy])
+                                                             (ig/ref [:syksy.web.index/handler ::syksy])
+                                                             (ig/ref [:syksy.web.resources/handler ::syksy])]
+                                                            addon-handlers
+                                                            [(ig/ref [:syksy.web.not-found/handler ::syksy])]))}
+   [:syksy.web.resources/handler ::syksy] {}
+   [:syksy.web.not-found/handler ::syksy] {}
+   [:syksy.web.index/handler ::syksy] {:index-body (or index-body (index/index {}))}
+   [:syksy.web.api/handler ::syksy] {:routes (or routes
+                                                (-> {:message "Syksy is ready"}
+                                                    (resp/ok)
+                                                    (constantly)))
+                                    :ctx ctx
+                                    :middleware middleware}})
