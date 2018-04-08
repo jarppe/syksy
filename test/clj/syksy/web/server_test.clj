bafdc8e692a8721c55543bc35a3ccdcd28d0de50
Jarppe <jarppe@metosin.fi>
2018-04-08 19:56:08 +0300

    Test all the features.

--- test/clj/syksy/web/server_test.clj
+++ test/clj/syksy/web/server_test.clj
@@ -1,25 +1,40 @@
 (ns syksy.web.server-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [testit.core :refer :all]
             [integrant.core :as ig]
             [clj-http.client :as http]
+            [jsonista.core :as json]
             [ring.util.http-response :as resp]
-            [syksy.web.index :as index]))
+            [potpuri.core :as p]
+            [syksy.core :as core]
+            [syksy.web.index :as index]
+            [syksy.web.server :as server]
+            [syksy.web.resources :as resources]
+            [syksy.web.redirect :as redirect]))
 
 (def ^:dynamic system nil)
 
+(def json-mapper (json/object-mapper {:encode-key-fn name
+                                      :decode-key-fn keyword}))
+
+
 (defn with-web-server [f]
-  (let [config {:syksy.web.server/server {:port 3001
-                                          :handlers [(ig/ref :syksy.web.api/handler)
-                                                     (ig/ref :syksy.web.index/handler)
-                                                     (ig/ref :syksy.web.resources/handler)
-                                                     (ig/ref :syksy.web.not-found/handler)]}
-                :syksy.web.api/handler {:routes (fn [request]
-                                                  (when (-> request :uri (= "/api"))
-                                                    (resp/ok {:hello "world"})))}
-                :syksy.web.index/handler {:index-body (index/index {:title "Syksy test"})}
-                :syksy.web.resources/handler {:asset-dir "server-test-resources"}
-                :syksy.web.not-found/handler {}}]
+  (let [ctx {:foo "bar"}
+        config (p/deep-merge
+                 (core/default-components {:index-body (index/index {:title "Syksy test"})
+                                           :routes (fn [request]
+                                                     (when (-> request :uri (= "/api"))
+                                                       (resp/ok {:hello "world"
+                                                                 :ctx (-> request :ctx)})))
+                                           :ctx ctx
+                                           :addon-handlers [(ig/ref [::resources/handler ::test])
+                                                            (ig/ref [::redirect/handler ::test])]})
+                 {[::server/server ::core/syksy] {:port 3001}
+                  [::resources/handler ::core/syksy] {:asset-dir "server-test-resources"}
+                  [::resources/handler ::test] {:asset-prefix "/addon"
+                                                :asset-dir "addon-resources"}
+                  [::redirect/handler ::test] {:from "/moved", :location "/here"}})]
     (ig/load-namespaces config)
     (let [s (ig/init config)]
       (try
@@ -30,24 +45,30 @@
 
 (use-fixtures :once with-web-server)
 
+(def opts {:throw-exceptions false
+           :redirect-strategy :none
+           :accept :json})
+
 (deftest api-is-served
   (fact
-    (http/get "http://localhost:3001/api" {:throw-exceptions false
-                                           :accept :json})
+    (-> (http/get "http://localhost:3001/api" opts)
+        (update :body json/read-value json-mapper))
     =in=> {:status 200
            :headers {"Content-Type" "application/json; charset=utf-8"
                      "cache-control" "no-store, must-revalidate"
                      "vary" "accept-encoding"}
-           :body "{\"hello\":\"world\"}"}))
+           :body {:hello "world"
+                  :ctx {:foo "bar"}}}))
 
 (deftest index-page-is-served
   (fact
-    (http/get "http://localhost:3001/")
+    (http/get "http://localhost:3001/" opts)
     =in=> {:status 200
            :headers {"Content-Type" "text/html; charset=utf-8"
                      "cache-control" "no-cache"
-                     "etag" (re-pattern #"\d+")}})
-  (let [etag (-> (http/get "http://localhost:3001/")
+                     "etag" (re-pattern #"\d+")}
+           :body (p/fn-> (str/index-of "title>Syksy test</title>") pos?)})
+  (let [etag (-> (http/get "http://localhost:3001/" opts)
                  :headers
                  (get "etag"))]
     (fact
@@ -59,27 +80,27 @@
 
 (deftest not-found-is-served
   (fact
-    (http/get "http://localhost:3001/foozaa" {:throw-exceptions false})
+    (http/get "http://localhost:3001/foozaa" opts)
     =in=> {:status 404}))
 
 (deftest resources-are-served
   (fact
-    (http/get "http://localhost:3001/asset/foo.css" {:throw-exceptions false})
+    (http/get "http://localhost:3001/asset/foo.css" opts)
     =in=> {:status 200
            :headers {"Content-Type" "text/css; charset=utf-8"
                      "cache-control" "no-cache"
                      "etag" (re-pattern #"\d+")}
            :body "* {\n  background: salmon;\n}\n\n"})
   (fact
-    (http/get "http://localhost:3001/asset/foo.js" {:throw-exceptions false})
+    (http/get "http://localhost:3001/asset/foo.js" opts)
     =in=> {:status 200
            :headers {"Content-Type" "application/javascript; charset=utf-8"}})
   (fact
-    (http/get "http://localhost:3001/asset/foo.txt" {:throw-exceptions false})
+    (http/get "http://localhost:3001/asset/foo.txt" opts)
     =in=> {:status 200
            :headers {"Content-Type" "text/plain; charset=utf-8"}})
   (fact
-    (http/get "http://localhost:3001/asset/foo.bar" {:throw-exceptions false})
+    (http/get "http://localhost:3001/asset/foo.bar" opts)
     =in=> {:status 404})
 
   (let [etag (-> (http/get "http://localhost:3001/asset/foo.txt")
@@ -91,3 +112,15 @@
     (fact
       (http/get "http://localhost:3001/asset/foo.txt" {:headers {"if-modified-since" "foo"}})
       =in=> {:status 200})))
+
+(deftest addon-resources-are-served
+  (fact
+    (http/get "http://localhost:3001/addon/bar.txt" opts)
+    =in=> {:status 200
+           :body "Hullo"}))
+
+(deftest redirect-is-observed
+  (fact
+    (http/get "http://localhost:3001/moved" opts)
+    =in=> {:status 307
+           :headers {"location" "/here"}}))
