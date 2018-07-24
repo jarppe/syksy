0a61e144bc9409e894c0a064c5e8d6c225a5c4ab
Jarppe <jarppe@metosin.fi>
2018-07-24 16:31:22 +0300

    Add support for uri-match? and uri-prefix options for index handler.

--- src/clj/syksy/core.clj
+++ src/clj/syksy/core.clj
@@ -1,7 +1,6 @@
 (ns syksy.core
   (:require [integrant.core :as ig]
-            [ring.util.http-response :as resp]
-            [syksy.web.index :as index]))
+            [ring.util.http-response :as resp]))
 
 (defn default-components [{:keys [host port index-body routes ctx middleware handlers addon-handlers]
                            :or {host "localhost"
@@ -9,17 +8,17 @@
   {[:syksy.web.server/server ::syksy] {:host host
                                        :port port
                                        :handlers (or handlers
-                                                    (concat [(ig/ref [:syksy.web.api/handler ::syksy])
-                                                             (ig/ref [:syksy.web.index/handler ::syksy])
-                                                             (ig/ref [:syksy.web.resources/handler ::syksy])]
-                                                            addon-handlers
-                                                            [(ig/ref [:syksy.web.not-found/handler ::syksy])]))}
+                                                     (concat [(ig/ref [:syksy.web.api/handler ::syksy])
+                                                              (ig/ref [:syksy.web.index/handler ::syksy])
+                                                              (ig/ref [:syksy.web.resources/handler ::syksy])]
+                                                             addon-handlers
+                                                             [(ig/ref [:syksy.web.not-found/handler ::syksy])]))}
    [:syksy.web.resources/handler ::syksy] {}
    [:syksy.web.not-found/handler ::syksy] {}
-   [:syksy.web.index/handler ::syksy] {:index-body (or index-body (index/index {}))}
+   [:syksy.web.index/handler ::syksy] {:index-body index-body}
    [:syksy.web.api/handler ::syksy] {:routes (or routes
-                                                (-> {:message "Syksy is ready"}
-                                                    (resp/ok)
-                                                    (constantly)))
-                                    :ctx ctx
-                                    :middleware middleware}})
+                                                 (-> {:message "Syksy is ready"}
+                                                     (resp/ok)
+                                                     (constantly)))
+                                     :ctx ctx
+                                     :middleware middleware}})
--- src/clj/syksy/web/index.clj
+++ src/clj/syksy/web/index.clj
@@ -5,13 +5,14 @@
             [hiccup.core :as hiccup]
             [hiccup.page :as page]
             [syksy.web.cache :as cache]
-            [syksy.util.checksum :as checksum]))
+            [syksy.util.checksum :as checksum]
+            [clojure.string :as str]))
 
 ;;
 ;; Index page:
 ;;
 
-(defn index [{:keys [title loading favicon]
+(defn index [{:keys [title loading favicon css js]
               :or {title "Another fancy Syksy app"
                    loading "Loading..."
                    favicon "/asset/favicon.ico"}}]
@@ -23,30 +24,41 @@
        [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
        [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
        [:link {:href favicon :rel "icon"}]
-       (page/include-css "/asset/css/style.css")]
+       (for [href (conj css "/asset/css/style.css")]
+         [:link {:href href
+                 :type "text/css"
+                 :rel "stylesheet"}])
+       (for [src js]
+         [:script {:src src
+                   :type "text/javascript"
+                   :deref true}])]
       [:body
        [:div#app
-        [:div.loading
-         [:h1 loading]]]
-       [:div#dev]
-       (page/include-js "/asset/js/main.js")])))
+        [:h1.loading loading]]
+       [:script {:src "/asset/js/main.js"
+                 :type "text/javascript"
+                 :deref true}]])))
 
 ;;
 ;; Index handler:
 ;;
 
-(defmethod ig/init-key ::handler [_ {:keys [index-body] :as opts}]
-  (let [index-hash (-> index-body checksum/hash-string str)
+(defmethod ig/init-key ::handler [_ {:keys [index-body uri-match? uri-prefix]}]
+  (let [index-hash (-> index-body (or (index {})) checksum/hash-string str)
         index-response (-> index-body
                            (resp/ok)
                            (resp/content-type "text/html; charset=utf-8")
                            (resp/header "etag" index-hash)
                            (resp/header cache/cache-control cache/cache-control-no-cache))
-        not-modified (resp/not-modified)]
+        not-modified (resp/not-modified)
+        uri-match? (or uri-match?
+                       (if uri-prefix
+                         (fn [uri] (str/starts-with? uri uri-prefix))
+                         (fn [uri] (= uri "/"))))]
     (log/infof "index handler: index-hash=%s" index-hash)
     (fn [request]
       (when (and (-> request :request-method (= :get))
-                 (-> request :uri (= "/")))
+                 (-> request :uri uri-match?))
         (if (-> request :headers (get cache/if-none-match) (= index-hash))
           not-modified
           index-response)))))
